adaf4a34405795850d8bd7b809a1cbf028811252
Maison Tiradoegas <maison@maisons-imac.attlocal.net>
2017-09-02 12:41:31 -0500

    End of Lesson 2.

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,21 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by Maison Tiradoegas on 8/30/17.
+//  Copyright © 2017 Maison Tiradoegas. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        
+    }
+
+    
+
+}
