665e0895a0f269c0d29c9b796c8f6ffdef38b549
Maison Tiradoegas <maison@maisons-imac.attlocal.net>
2017-09-04 00:44:40 -0500

    Lesson 4.

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Maison Tiradoegas on 9/3/17.
+//  Copyright © 2017 Maison Tiradoegas. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+    }
+
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Maison Tiradoegas on 9/3/17.
+//  Copyright © 2017 Maison Tiradoegas. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+
+let TO_LOGIN = "toLogin"
